2fb6dc497c254e4fb5a70dc6979007b2b90cc013
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-10-20 18:40:21 +0200

    find html element by class name

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,9 +2,16 @@
 document.getElementById("demo1").innerHTML = "hello World!";
 //to find html element by id
 const element = document.getElementById("intro");
-document.getElementById("demo").innerHTML =  element.innerHTML;
+
+document.getElementById("demo").innerHTML = 
+"The text from the intro paragraph is: " + element.innerHTML;
 
 // finding html element by tag name
 
 const elements = document.getElementsByTagName("p");
 document.getElementById("demo").innerHTML = elements[0].innerHTML;
+
+// finding html element by class name
+
+const x = document.getElementsByClassName("intro1");
+document.getElementById("demo").innerHTML = x[0].innerHTML;
