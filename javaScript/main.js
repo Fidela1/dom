5509a32b3f2854f30843ddfa0576a632d098c6fc
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-10-20 17:45:03 +0200

    new changes

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,3 +2,4 @@
 document.getElementById("demo").innerHTML = "hello World!";
 document.getElementById("demo1").innerHTML = "hello World!";
 document.getElementById("demo2").innerHTML = "hello World!";
+document.getElementById("demo2").innerHTML = "hello";
