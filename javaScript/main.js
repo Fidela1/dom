5d058a3bd069baef7799a53c4e95eb9aaa837ba2
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-10-20 18:23:15 +0200

    finding element

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,10 @@
 
-document.getElementById("demo").innerHTML = "hello World!";
+document.getElementById("demo1").innerHTML = "hello World!";
 //to find html element by id
 const element = document.getElementById("intro");
 document.getElementById("demo").innerHTML =  element.innerHTML;
+
+// finding html element by tag name
+
+const elements = document.getElementsByTagName("p");
+document.getElementById("demo").innerHTML = elements[0].innerHTML;
